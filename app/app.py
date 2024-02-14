95c6be6b14604e5428bf3cae7fc62dfea81ca507
eceechain <jaredamima4@gmail.com>
2024-02-14 18:29:28 +0300

    Created Users routes

--- app/app.py
+++ app/app.py
@@ -1,12 +1,15 @@
+import datetime
 from flask import Flask, jsonify, request
+from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token, get_jwt_identity
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_cors import CORS
-from models import db, User
+from models import CalorieLog, WorkoutLog, db, User
 
 app = Flask(__name__)
 CORS(app)
 jwt = JWTManager(app)
+migrate = Migrate(app, db)
 
 # Setting up Flask JWT
 app.config['JWT_SECRET_KEY'] = 'SECRET'
@@ -14,8 +17,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
-
-
 
 # Protected route requiring JWT authentication
 @app.route('/login', methods=['POST'])
@@ -42,5 +43,55 @@ def protected():
     current_user = get_jwt_identity()
     return jsonify(logged_in_as=current_user), 200
 
+# Register route
+@app.route('/Register', methods=['POST'])
+def register():
+    data = request.get_json()
+    username = data.get('username')
+    email = data.get('email')
+    password = data.get('password')
+
+# Check if the user already exists
+    user = User.query.filter_by(username=username).first()
+    if user:
+        return jsonify(message='User already exists'), 409
+    else:
+        user = User(username=username, email=email)
+        user.set_password(password)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify(message='User created successfully'), 201
+
+# Get all users
+@app.route('/Users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    users_list = []
+    for user in users:
+        user_data = {}
+        user_data['id'] = user.id
+        user_data['username'] = user.username
+        user_data['email'] = user.email
+        user_data['password_hash'] = user.password_hash
+        user_data['is_active'] = user.is_active
+        users_list.append(user_data)
+    return jsonify(users=users_list), 200
+
+
+# Get a single user
+@app.route('/Users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    user = User.query.get(user_id)
+    if user:
+        user_data = {}
+        user_data['id'] = user.id
+        user_data['username'] = user.username
+        user_data['email'] = user.email
+        user_data['password_hash'] = user.password_hash
+        user_data['is_active'] = user.is_active
+        return jsonify(user=user_data), 200
+    else:
+        return jsonify(message='User not found'), 404
+
 if __name__ == '__main__':
-    app.run(debug=True, port=5005)
+    app.run(debug=True, port=5002)
