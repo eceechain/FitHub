050b701eeaa04a7fb3a5815bdf37b3f2ac481c96
abvss13 <abdullahi.abass@student.moringaschool.com>
2024-02-17 21:07:25 +0300

    improving  activity section

--- client/src/Components/Home/Activities.js
+++ client/src/Components/Home/Activities.js
@@ -4,21 +4,29 @@ import './Activities.css';
 function Activities() {
   return (
     <div className="activities-container">
-      <h1>Explore Exciting Activities</h1>
+      <h1>Explore Workout Activities</h1>
       <div className="activity">
-        <img src="image_url_here" alt="Activity 1" className="activity-image" />
+        <img src="http://tinyurl.com/2v9bxc5m" alt="Weightlifting" className="activity-image" />
         <div className="activity-details">
-          <h2>Activity 1</h2>
-          <p>Description of Activity 1 Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          <a href="/activity1">Learn more</a>
+          <h2>Weightlifting</h2>
+          <p>Description of Weightlifting: Build strength and muscle with heavy lifting and resistance training.</p>
+          <a href="/weightlifting">Learn more</a>
         </div>
       </div>
       <div className="activity">
-        <img src="image_url_here" alt="Activity 2" className="activity-image" />
+        <img src="http://tinyurl.com/yck9akk9" alt="Boxing" className="activity-image" />
         <div className="activity-details">
-          <h2>Activity 2</h2>
-          <p>Description of Activity 2 Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          <a href="/activity2">Learn more</a>
+          <h2>Boxing</h2>
+          <p>Description of Boxing: Train your body and mind while learning the art of self-defense.</p>
+          <a href="/boxing">Learn more</a>
+        </div>
+      </div>
+      <div className="activity">
+        <img src="https://via.placeholder.com/300" alt="Cardio" className="activity-image" />
+        <div className="activity-details">
+          <h2>Cardio</h2>
+          <p>Description of Running: Conquer the roads and challenge yourself with this thrilling pace.</p>
+          <a href="/rock-climbing">Learn more</a>
         </div>
       </div>
       {/* Add more activities here */}
