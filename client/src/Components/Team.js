c248224985c467861d3653bc38d27888a75edc71
abvss13 <abdullahi.abass@student.moringaschool.com>
2024-02-21 21:42:17 +0300

    adding teams components

--- client/src/Components/Team.js
+++ client/src/Components/Team.js
@@ -1,14 +1,22 @@
-// Team.js
+// FAQs.js
 import React from 'react';
-import './Team.css';
+import './FAQs.css';
 
-const Team = () => {
-    return (
-        <div className="team">
-            <h2>Team</h2>
-            {/* Add content here */}
-        </div>
-    );
+const FAQs = () => {
+  return (
+    <div className="FAQs">
+      <h2>Frequently Asked Questions</h2>
+      <p>Here are some common questions about FitHub:</p>
+      <ul>
+        <li>What features does FitHub offer?</li>
+        <li>How much does FitHub cost?</li>
+        <li>Is FitHub compatible with my device?</li>
+        <li>How do I cancel my FitHub subscription?</li>
+        <li>Where can I find technical support for FitHub?</li>
+      </ul>
+      <p>If you have any other questions, feel free to contact us for assistance.</p>
+    </div>
+  );
 }
 
-export default Team;
+export default FAQs;
