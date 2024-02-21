f662d50df1afdd0d9ec15fddd0b34634a7d28368
abvss13 <abdullahi.abass@student.moringaschool.com>
2024-02-21 21:40:48 +0300

    adding FAQS

--- client/src/Components/FAQs.js
+++ client/src/Components/FAQs.js
@@ -3,12 +3,20 @@ import React from 'react';
 import './FAQs.css';
 
 const FAQs = () => {
-    return (
-        <div className="faqs">
-            <h2>FAQs</h2>
-            {/* Add content here */}
-        </div>
-    );
+  return (
+    <div className="FAQs">
+      <h2>Frequently Asked Questions</h2>
+      <p>Here are some common questions about FitHub:</p>
+      <ul>
+        <li>What features does FitHub offer?</li>
+        <li>How much does FitHub cost?</li>
+        <li>Is FitHub compatible with my device?</li>
+        <li>How do I cancel my FitHub subscription?</li>
+        <li>Where can I find technical support for FitHub?</li>
+      </ul>
+      <p>If you have any other questions, feel free to contact us for assistance.</p>
+    </div>
+  );
 }
 
 export default FAQs;
