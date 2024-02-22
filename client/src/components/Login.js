f7715a39ff4fef06d755360073ed010c9c757c01
alinoor12345 <118164799+alinoor12345@users.noreply.github.com>
2024-02-22 11:44:13 +0300

    added the Log In button

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -17,6 +17,7 @@ const Login = (props) => {
             <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
             <label htmlFor="password">password</label>
             <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="****" id="password" name="password" />
+            <button type="submit">Log In</button>
         </form>
 
     </div> 
