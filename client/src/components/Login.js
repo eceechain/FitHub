9b1c4ec091c06137a6e9b5ca9af6e7f8cdfadd42
alinoor12345 <118164799+alinoor12345@users.noreply.github.com>
2024-02-22 10:21:51 +0300

    improving the login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,13 @@
 import React, { useState } from 'react';
 
 const LoginForm = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+}
+const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
