2eb83bff137e28b14579d245057132d55f5748f4
alinoor12345 <118164799+alinoor12345@users.noreply.github.com>
2024-02-22 12:02:53 +0300

    added the link button

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -19,6 +19,7 @@ const Login = (props) => {
             <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="****" id="password" name="password" />
             <button type="submit">Log In</button>
         </form>
+        <button className="link-btn" onClick={() => props.onFormSwitch('register')}>Don't have an account? Register here.</button>
 
     </div> 
     )
