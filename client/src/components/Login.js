daea1d333b414d98dab8ebd9eb107c2da4ecaaa9
alinoor12345 <118164799+alinoor12345@users.noreply.github.com>
2024-02-22 11:40:26 +0300

    resolved the return error to make the code work

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -3,24 +3,29 @@ import React, { useState } from "react";
 const Login = (props) => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
-}
-const handleSubmit = (e) => {
+
+    const handleSubmit = (e) => {
     e.preventDefault();
     console.log(email);
-}
+    }
 
-return (
+    return (
     <div className="auth-form-container">
         <h2>Login</h2>
         <form className="login-form" onSubmit={handleSubmit}>
             <label htmlFor="email">email</label>
             <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="youremail@gmail.com" id="email" name="email" />
             <label htmlFor="password">password</label>
-
+            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="****" id="password" name="password" />
         </form>
 
-    </div>
-)
+    </div> 
+    )
+}
+
+export  default Login;          
+
+
   
 
 
