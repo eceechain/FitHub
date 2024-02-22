fb40f17b095b155df6f25d22ec12d2a78d9f1a37
alinoor12345 <118164799+alinoor12345@users.noreply.github.com>
2024-02-22 10:46:29 +0300

    improved the login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -4,7 +4,6 @@ const Login = (props) => {
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
 }
-
 const handleSubmit = (e) => {
     e.preventDefault();
     console.log(email);
