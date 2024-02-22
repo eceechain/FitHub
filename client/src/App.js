d9d66b36ec960a8f8681cb4f14c70ee2402e03df
alinoor12345 <118164799+alinoor12345@users.noreply.github.com>
2024-02-22 10:12:53 +0300

    setting up login components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginForm from './components/Login';
 
 function App() {
   return (
---
+++ client/src/components/Login.js
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
