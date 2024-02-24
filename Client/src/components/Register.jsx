2e8a079205d89dc7f19c42e9ca3ce257b93c2d16
alinoor12345 <118164799+alinoor12345@users.noreply.github.com>
2024-02-24 23:44:43 +0300

    refactored sig-up page

--- Client/src/components/Register.jsx
+++ Client/src/components/Register.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-function Register() {
+export default function Register() {
+
+
   return (
-    <div>Register</div>
-  )
+    <div className="wrapper signUp">
+      <div className="illustration">
+        {/* <img src="https://source.unsplash.com/random" alt="illustration" /> */}
+      </div>
+      <div className="form">
+        <div className="heading">CREATE AN ACCOUNT</div>
+        <form>
+          <div>
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" placeholder="Enter your name" />
+          </div>
+          <div>
+            <label htmlFor="emial">E-Mail</label>
+            <input type="text" id="email" placeholder="Enter your mail" />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Enter you password"
+            />
+          </div>
+          <button type="submit">Submit</button>
+          <h2 align="center" className="or">
+            OR
+          </h2>
+        </form>
+        <p>
+          Have an account ? <Link to="/account/login"> Login </Link>
+        </p>
+      </div>
+    </div>
+  );
 }
-
-export default Register
