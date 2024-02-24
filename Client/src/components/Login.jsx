0e7df0abe0124931c758f945e7489c65c7ae70c5
alinoor12345 <118164799+alinoor12345@users.noreply.github.com>
2024-02-25 00:12:55 +0300

    test login api

--- Client/src/components/Login.jsx
+++ Client/src/components/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useAuthContext } from './App';
 
@@ -6,13 +6,47 @@ import { useAuthContext } from './App';
 export default function Login() {
   const navigate = useNavigate();
   const {isAuthenticated, setIsAuthenticated} = useAuthContext();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   console.log(isAuthenticated);
-  
+
   const preventRefresh = (e) => {
     e.preventDefault();
     setIsAuthenticated(true);
     navigate('/');
   };
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setIsLoading(true);
+
+    // Send login request to the server
+    fetch("http://localhost:5001/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username: "john", password: "password123" }),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Invalid credentials");
+        }
+        return res.json();
+      })
+      .then((user) => {
+        // onLogin(user);
+        // navigate("/exercises");
+        console.log(user);
+      })
+      .catch((error) => {
+        alert(error.message);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }
 	return (
 		<div className="wrapper signIn">
 			<div className="illustration">
@@ -29,7 +63,7 @@ export default function Login() {
 						<label htmlFor="password">Password</label>
 						<input type="password" id="password" placeholder="****************" />
 					</div>
-					<button type="submit" onClick={preventRefresh}>
+					<button type="submit" onClick={handleSubmit}>
 						Submit
 					</button>
 				</form>
--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import '../styles/Navbar.css';
+import { useAuthContext } from './App';
 
 function Navbar() {
+  const {isAuthenticated, setIsAuthenticated} = useAuthContext();
+  console.log(isAuthenticated);
   return (
     <nav className="navbar">
       <div className="nav-links">
@@ -17,9 +20,11 @@ function Navbar() {
         <NavLink to="/blog">Blog</NavLink>
       </div>
       <div className="auth-links">
+        {isAuthenticated ? <button onClick={() => setIsAuthenticated(false)}>logout</button> : <>
         <NavLink to="/socialsharing">Social Sharing</NavLink>
         <NavLink to="/account/login">Login</NavLink>
         <NavLink to="/account/register">Register</NavLink>
+        </>}
       </div>
     </nav>
   );
