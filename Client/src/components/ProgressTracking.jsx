2234b6d8d24d1481e6c1117d924497ec03881ac7
abvss13 <abdullahi.abass@student.moringaschool.com>
2024-02-25 21:16:05 +0300

    css for progress tracking

--- Client/src/components/ProgressTracking.jsx
+++ Client/src/components/ProgressTracking.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import '../styles/ProgressTracking.css'
 
 function ProgressTracking() {
   const [progressData, setProgressData] = useState([]);
