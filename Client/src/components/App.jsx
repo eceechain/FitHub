22d1bcfa4d70f7708703f25f2b8f45cd9b3ddda6
alinoor12345 <118164799+alinoor12345@users.noreply.github.com>
2024-02-25 15:17:35 +0300

    save access token to local storage

--- Client/src/components/App.jsx
+++ Client/src/components/App.jsx
@@ -1,5 +1,4 @@
-import React, {createContext, useContext} from 'react'
-import { useState } from 'react';
+import React, {createContext, useContext, useState, useEffect} from 'react'
 import Dashboard from './Dashboard'
 
 const AuthContext = createContext({
@@ -10,10 +9,32 @@ export const useAuthContext = () => useContext(AuthContext);
 
 function App() {
   const  [isAuthenticated, setIsAuthenticated] = useState(false);
+  const  [accessToken, setAccessToken] = useState(localStorage.getItem('acess_token'));
+
+  const saveAcessToken = (token) => {
+    setAccessToken(token);
+    localStorage.setItem('acess_token', token);
+    setIsAuthenticated(true);
+  };
+
+  const removeAcessToken = () => {
+    setAccessToken(null);
+    localStorage.removeItem('acess_token');
+    setIsAuthenticated(false);
+  };
+
+  useEffect(() => {
+    if (accessToken) setIsAuthenticated(true);
+  }, []);
+
   return (
     <AuthContext.Provider value={{
       isAuthenticated,
       setIsAuthenticated,
+      accessToken,
+      setAccessToken,
+      saveAcessToken,
+      removeAcessToken
     }}>
       <Dashboard />
     </AuthContext.Provider>
