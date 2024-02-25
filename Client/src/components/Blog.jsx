5b912ade1692882965c3076763f560b1929b1258
abvss13 <abdullahi.abass@student.moringaschool.com>
2024-02-25 17:26:10 +0300

    initial creation of blog component

--- Client/src/components/Blog.jsx
+++ Client/src/components/Blog.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
 
 function Blog() {
   return (
-    <div>Blog</div>
-  )
+    <div className="blog-container">
+      <h2>Welcome to the FitHub Blog!</h2>
+      <p>FitHub is based in Nairobi, Kenya, and located at Ngong Lane Plaza. It was founded and created by Abdullahi Abass, Jared Amima, Ali Nur, Leroy Kariuki, Isaac Mutiga, and Craig Otieno, who are graduates from Moringa School, a leading tech bootcamp.</p>
+      <h3>Latest Posts</h3>
+      <div className="blog-post">
+        <h4>Post Title 1</h4>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut est velit. Suspendisse potenti. Nam vehicula faucibus diam, in fringilla ligula porta et.</p>
+        <img src="post1.jpg" alt="Post 1" />
+      </div>
+      <div className="blog-post">
+        <h4>Post Title 2</h4>
+        <p>Integer et convallis eros, non ultricies ligula. Nulla facilisi. Suspendisse eget est orci. Fusce vestibulum ultrices sapien, non fringilla libero.</p>
+        <img src="post2.jpg" alt="Post 2" />
+      </div>
+      <div className="blog-post">
+        <h4>Post Title 3</h4>
+        <p>Vestibulum nec aliquet ligula, id rutrum urna. Nulla facilisi. Morbi rhoncus urna id risus vehicula, ut lacinia ligula posuere.</p>
+        <img src="post3.jpg" alt="Post 3" />
+      </div>
+    </div>
+  );
 }
 
-export default Blog
+export default Blog;
--- Client/src/components/Home.jsx
+++ Client/src/components/Home.jsx
@@ -17,7 +17,7 @@ function Home() {
   <h2 className="intro-heading">Your All-in-One Fitness Tracking App</h2>
   <p className="intro-text">Take control of your fitness journey with FitHub. Whether you're aiming to lose weight, build muscle, or simply stay active, FitHub provides you with the tools and support you need to achieve your goals.</p>
   <p className="intro-text">Track your workouts, monitor your calorie intake, set personalized goals, and get expert recommendations tailored to your fitness level and objectives. With FitHub, you can access a vast library of exercises, healthy recipes, and connect with a supportive fitness community.</p>
-  <Link to="/register" className="get-started-button">Get Started</Link>
+  <Link to="/WorkoutLogs" className="get-started-button">Get Started</Link>
 </div>
 
 
