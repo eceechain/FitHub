5eb62823e7ab61879a1fb45f5f0509bc70c69ef8
abvss13 <abdullahi.abass@student.moringaschool.com>
2024-02-25 10:47:13 +0300

    improving on the side bar menu

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { FaBars, FaTimes } from 'react-icons/fa';
 import '../styles/Navbar.css';
 
 function Navbar() {
+  const [showSidebar, setShowSidebar] = useState(false);
+
+  const toggleSidebar = () => {
+    setShowSidebar(!showSidebar);
+  };
+
   return (
     <nav className="navbar">
       <div className="nav-links">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/workoutlogs">Workout Logs</NavLink>
-        <NavLink to="/progress">Progress Tracking</NavLink>
-        <NavLink to="/calories">Calories Tracking</NavLink>
-        <NavLink to="/recommendations">Personalized Recommendations</NavLink>
-        <NavLink to="/goalsetting">Goal Setting</NavLink>
-        <NavLink to="/about">About Us</NavLink>
-        <NavLink to="/contact">Contact us</NavLink>
-        <NavLink to="/blog">Blog</NavLink>
+        <NavLink to="/" className="logo">
+          FitnessHub
+        </NavLink>
+      </div>
+      <div className="sidebar-toggle" onClick={toggleSidebar}>
+        {showSidebar ? <FaTimes /> : <FaBars />}
+      </div>
+      <div className={`sidebar ${showSidebar ? 'active' : ''}`}>
+        <div className="sidebar-links">
+          <NavLink to="/dashboard" onClick={toggleSidebar}>Dashboard</NavLink>
+          <NavLink to="/workoutlogs" onClick={toggleSidebar}>Workout Logs</NavLink>
+          <NavLink to="/progress" onClick={toggleSidebar}>Progress Tracking</NavLink>
+          <NavLink to="/calories" onClick={toggleSidebar}>Calories Tracking</NavLink>
+          <NavLink to="/recommendations" onClick={toggleSidebar}>Personalized Recommendations</NavLink>
+          <NavLink to="/goalsetting" onClick={toggleSidebar}>Goal Setting</NavLink>
+          <NavLink to="/socialsharing" onClick={toggleSidebar}>Social Sharing</NavLink>
+          <NavLink to="/blog" onClick={toggleSidebar}>Blog</NavLink>
+        </div>
       </div>
       <div className="auth-links">
-        <NavLink to="/socialsharing">Social Sharing</NavLink>
         <NavLink to="/account/login">Login</NavLink>
         <NavLink to="/account/register">Register</NavLink>
       </div>
