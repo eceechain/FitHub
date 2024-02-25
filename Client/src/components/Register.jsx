5f43dac43c240cfe86afcff5a6eb65387d79b332
alinoor12345 <118164799+alinoor12345@users.noreply.github.com>
2024-02-25 15:13:53 +0300

    register new users

--- Client/src/components/Register.jsx
+++ Client/src/components/Register.jsx
@@ -1,7 +1,47 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuthContext } from "./App";
 
 export default function Register() {
+
+  const navigate = useNavigate();
+  const {saveAcessToken} = useAuthContext();
+  const [name, setname] = useState("");
+  const [email, setemail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  console.log(name);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setIsLoading(true);
+
+    // Send register request to the server
+    fetch("https://fithub-kl23.onrender.com/Register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username: name, email: email, password: password }),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Invalid credentials");
+        }
+        return res.json();
+      })
+      .then((result) => {
+        // onLogin(user);
+        saveAcessToken(result.access_token);
+        navigate('/');
+      })
+      .catch((error) => {
+        alert(error.message);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }
 
 
   return (
@@ -11,24 +51,26 @@ export default function Register() {
       </div>
       <div className="form">
         <div className="heading">CREATE AN ACCOUNT</div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label htmlFor="name">Name</label>
-            <input type="text" id="name" placeholder="Enter your name" />
+            <input required type="text" id="name" placeholder="Enter your name" value={name} onChange={(e) => setname(e.target.value)} />
           </div>
           <div>
             <label htmlFor="emial">E-Mail</label>
-            <input type="text" id="email" placeholder="Enter your mail" />
+            <input required type="text" id="email" placeholder="Enter your mail" value={email} onChange={(e) => setemail(e.target.value)} />
           </div>
           <div>
             <label htmlFor="password">Password</label>
             <input
+              required
               type="password"
               id="password"
               placeholder="Enter you password"
+              value={password} onChange={(e) => setPassword(e.target.value)}
             />
           </div>
-          <button type="submit">Submit</button>
+          <button type="submit">{isLoading ? "Please wait..." : "Submit"}</button>
           <h2 align="center" className="or">
             OR
           </h2>
