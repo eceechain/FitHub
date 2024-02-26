ee818bf2cd78818f3f1872d7a48d61ab47a72741
abvss13 <abdullahi.abass@student.moringaschool.com>
2024-02-26 07:35:26 +0300

    Added CSS styles for the WorkoutLogs component.

--- Client/src/components/WorkoutLogs.jsx
+++ Client/src/components/WorkoutLogs.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import '../styles/WorkoutLog.css'
 
 function WorkoutLogs() {
   const [workouts, setWorkouts] = useState([]);
