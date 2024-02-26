bd57a1ba289cf9054df3f438a368bdc749bb2e9e
abvss13 <abdullahi.abass@student.moringaschool.com>
2024-02-26 07:31:36 +0300

    craeted workout log components

--- Client/src/components/WorkoutLogs.jsx
+++ Client/src/components/WorkoutLogs.jsx
@@ -1,40 +1,51 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import '../styles/WorkoutLog.css'
 
 function WorkoutLogs() {
-  const [workoutLogs, setWorkoutLogs] = useState([]);
+  const [workouts, setWorkouts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    axios.get('https://api.npoint.io/74b3e41d7d5439546e8c')
-      .then(response => {
-        setWorkoutLogs(response.data.workoutLogs);
-      })
-      .catch(error => {
-        console.error('Error fetching workout logs:', error);
-      });
+    fetchData();
   }, []);
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get('https://fithub-kl23.onrender.com/Workouts');
+      setWorkouts(response.data.workouts);
+      setLoading(false);
+    } catch (error) {
+      setError('Error fetching workout data. Please try again later.');
+      setLoading(false);
+      console.error('Error fetching workout data:', error);
+    }
+  };
 
   return (
     <div className="workout-logs-container">
       <h2>Workout Logs</h2>
-      <ul>
-        {workoutLogs.map(log => (
-          <li key={log.id} className="workout-log">
-            <div className="exercise">
-              <strong>{log.exercise}</strong>
+      {loading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      {!loading && !error && (
+        <div className="workout-list">
+          {workouts.map(workout => (
+            <div key={workout.id} className="workout-item">
+              <img src={workout.image} alt={workout.description} className="workout-image" />
+              <div className="workout-details">
+                <h3>{workout.workout_type}</h3>
+                <p>{workout.description}</p>
+                <p>Date: {workout.date}</p>
+                <p>Duration: {workout.duration} minutes</p>
+                <p>Sets: {workout.sets}</p>
+                <p>Reps: {workout.reps}</p>
+                <p>Calories Burned: {workout.calories_burned}</p>
+              </div>
             </div>
-            <div className="details">
-              {log.duration && <p>Duration: {log.duration}</p>}
-              {log.sets && <p>Sets: {log.sets}</p>}
-              {log.reps && <p>Reps: {log.reps}</p>}
-              {log.distance && <p>Distance: {log.distance}</p>}
-              <p>Date: {log.date}</p>
-            </div>
-            {log.image && <img src={log.image} alt={log.exercise} className="exercise-image" />}
-          </li>
-        ))}
-      </ul>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
