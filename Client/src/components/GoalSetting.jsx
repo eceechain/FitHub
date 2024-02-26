67e8256968ff6b29b8aa8794d999ed336a954875
abvss13 <abdullahi.abass@student.moringaschool.com>
2024-02-26 06:00:31 +0300

    creating goal setting component

--- Client/src/components/GoalSetting.jsx
+++ Client/src/components/GoalSetting.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function GoalSetting() {
+  const [goals, setGoals] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('https://fithub-kl23.onrender.com/GoalSetting');
+        setGoals(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div>GoalSetting</div>
-  )
+    <div>
+      <h2>Goal Setting</h2>
+      <p>Allow users to set specific fitness goals, such as weight loss, muscle gain, or improving endurance, and track their progress towards these goals.</p>
+      <div>
+        {goals.map(goal => (
+          <div key={goal.id}>
+            <h3>Goal Type: {goal.goal_type}</h3>
+            <p>Deadline: {goal.deadline}</p>
+            <p>Target: {goal.target}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default GoalSetting
+export default GoalSetting;
